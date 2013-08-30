6cc78fc89f1c08a9128394f6f100ac2bfa856baa
ecl1pse <ecl1pse.armageddon@gmail.com>
2013-08-30 16:32:17 -0700

    Rewrite the form for now until Deface can be figured out

--- app/overrides/add_to_cart.rb
+++ app/overrides/add_to_cart.rb
@@ -1,14 +1,14 @@
-Deface::Override.new(
-  :virtual_path => 'spree/products/_cart_form',
-  :name => 'modify_max_add_to_cart',
-  :replace_contents => ".add-to-cart",
-  :text => "
-          <%= number_field_tag (@product.variants_and_option_values.any? ? :quantity : :quantity),
-            1, :class => 'title', :min => 1, :max => @product.limit_qty %>
-          <%= button_tag :class => 'large primary', :id => 'add-to-cart-button', :type => :submit do %>
-            <%= Spree.t(:add_to_cart) %>
-          <% end %>
-  ")
+# Deface::Override.new(
+#   :virtual_path => 'spree/products/_cart_form',
+#   :name => 'modify_max_add_to_cart',
+#   :replace_contents => ".add-to-cart",
+#   :text => "
+#           <%= number_field_tag (@product.variants_and_option_values.any? ? :quantity : 'variants[#{@product.master.id}]\'),
+#             1, :class => 'title', :min => 1, :max => @product.limit_qty %>
+#           <%= button_tag :class => 'large primary', :id => 'add-to-cart-button', :type => :submit do %>
+#             <%= Spree.t(:add_to_cart) %>
+#           <% end %>
+#   ")
 # 
 # Deface::Override.new(
 #   :virtual_path => 'spree/products/_cart_form',
