e9fcf9edb17abb6b20ca1af5b8daa22b3e979ec7
Nozim Mehrubonov <nozim.mehrubonov@gmail.com>
2013-10-24 21:11:12 +0600

    Adding one more scenario for limit quantity feature

--- spec/features/admin/limit_qty_spec.rb
+++ spec/features/admin/limit_qty_spec.rb
@@ -1,8 +1,28 @@
 require 'spec_helper'
 
-feature 'Limit qty' do
+feature 'Limit qty when product has variant' do
   stub_authorization!
+  
+  let!(:variant) { create(:variant)}
+  let!(:product) { variant.product }
 
+  background do
+    sign_in_as!(create(:admin_user))
+    visit spree.admin_path
+    click_link "Products"
+  end
+
+  scenario "Adding a limit qty" do
+    click_link product.name
+    fill_in 'Limit Qty', with: 5
+    click_button "Update"
+    find_field('Limit Qty').value.should eql("5")
+  end
+end
+
+feature 'Limit qty when product hasn\'t variant' do
+  stub_authorization!
+  
   let!(:product) { create(:product) }
 
   background do
